76fea49e15725852fd7439f36a0ea813164bfe04
CuiYao <631836754@qq.com>
2024-03-04 15:16:12 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
+import Home from './components/home/home'
 
 function App() {
     return (
         <div id="App">
             <BrowserRouter>
                 <Routes>
-
+                    <Route path="/" element={<Home/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
