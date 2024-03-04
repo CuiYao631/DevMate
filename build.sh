9e10643261ade72fac7e4c034ce63bee90308ee9
CuiYao <631836754@qq.com>
2024-03-04 13:46:47 +0800

    update

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ test -f DevMate-Installer.dmg && rm DevMate-Installer.dmg
 wails build -u -platform darwin/universal
 create-dmg \
     --volname "DevMate Installer" \
-    --volicon "build/bin/matedev.app/Contents/Resources/iconfile.icns" \
+    --volicon "build/bin/DevMate/Contents/Resources/iconfile.icns" \
     --icon-size 75 \
     --window-size 600 400 \
     --icon "DevMate.app" 200 170 \
